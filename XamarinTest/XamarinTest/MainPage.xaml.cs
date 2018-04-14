c20025cac57dbbd646bd9a7773c0bdf3e3971f23
Myles-Glenn Gaerlan <m.gaerlan@live.com>
2018-04-15 06:06:53 +1000

    Initial Commit

---
+++ XamarinTest/XamarinTest/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinTest
+{
+	public partial class MainPage : ContentPage
+	{
+        QuotesPage qp = new QuotesPage();
+
+        public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            quoteLabel.Text = qp.GenerateQuote();
+        }
+    }
+}
---
+++ XamarinTest/XamarinTest/QuotesPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinTest
+{
+    public class QuotesPage
+    {
+        private int number;
+        private string[] quotes;
+        private Random random;
+
+        public QuotesPage()
+        {
+            random = new Random();
+            quotes = new string[] 
+            {
+                "Do not take life too seriously - you will never get out of it alive!",
+                "Everything is funny, as long as it's happening to someone else.",
+                "Weather forecast for tonight: Dark.",
+                "Electricity is really just organised lightning.",
+                "I would never die for my beliefs because I might be wrong."
+            };
+        }
+
+        public string GenerateQuote()
+        {
+            number = random.Next(1,5);
+            return quotes[number];
+        }
+    }
+}
